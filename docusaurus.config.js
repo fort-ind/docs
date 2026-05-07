b35b4c99cb2250ed506153017c5a6dfab0e86580
Ranvir Singh <100595269+rynizx@users.noreply.github.com>
2026-05-07 21:48:44 +0000

    ooo lala

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,6 +81,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       colorMode: {
+        defaultMode: 'dark',
         respectPrefersColorScheme: true,
       },
       navbar: {
